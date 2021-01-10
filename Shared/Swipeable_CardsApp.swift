dd054e7f074d3a25915e7acf87191b63a3ca24aa
Kris Reid <krisreid_06@hotmail.com>
2021-01-10 09:38:55 +0000

    Initial Commit

---
+++ Shared/Swipeable_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipeable_CardsApp.swift
+//  Shared
+//
+//  Created by Kris Reid on 10/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Swipeable_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
